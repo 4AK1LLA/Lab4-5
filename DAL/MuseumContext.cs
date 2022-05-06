b04e9d81ec2bf8a95a8e46107407665fe9f77b01
Ruslan Tretyakov <ruslan.tretyakov2003@gmail.com>
2022-05-06 15:37:43 +0300

    Added DbContext

---
+++ DAL/MuseumContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DAL.Entities;
+
+namespace DAL
+{
+    public class MuseumContext : DbContext
+    {
+        public MuseumContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Exhibition>? Exhibitions { get; set; }
+        public DbSet<Excursion>? Excursions { get; set; }
+    }
+}
