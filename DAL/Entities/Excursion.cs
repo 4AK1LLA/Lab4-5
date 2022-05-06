a60f958ac20e0c40a5500c46d44263ad1ea43348
Ruslan Tretyakov <ruslan.tretyakov2003@gmail.com>
2022-05-06 15:26:43 +0300

    Added entities to DAL

---
+++ DAL/Entities/Excursion.cs
@@ -0,0 +1,11 @@
+﻿namespace DAL.Entities
+{
+    public class Excursion
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public DateTime Time { get; set; }
+        public bool IsReserved { get; set; }
+    }
+}
---
+++ DAL/Entities/Exhibition.cs
@@ -0,0 +1,13 @@
+﻿namespace DAL.Entities
+{
+    public class Exhibition
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public double Price { get; set; }
+        public int NumberOfVisitors { get; set; }
+        public DateTime Beginning { get; set; }
+        public DateTime End { get; set; }
+    }
+}
