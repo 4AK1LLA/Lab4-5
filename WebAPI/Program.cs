312351c91604a643d51c32529266f69ab8437d4c
Ruslan Tretyakov <ruslan.tretyakov2003@gmail.com>
2022-05-06 18:45:34 +0300

    Added connection string

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,7 +1,12 @@
+using DAL;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<MuseumContext>(options =>
+       options.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
